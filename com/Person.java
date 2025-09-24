b28eaede51fff289c888f923aa56bd731948c3be
PPR2006 <praneethreddypolam06@gmail.com>
2025-09-24 12:42:10 +0530

    Add files via upload

---
+++ com/Person.java
@@ -0,0 +1,14 @@
+//Abstraction 
+package Student;
+
+abstract class Person{
+	protected String name;
+	
+	Person (String name){
+		this.name = name;
+	}
+	abstract String getDetails();   //Abstraction method
+}  
+
+
+
---
+++ com/Studentmain.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+package Student;
+import java.io.*;
+
+class studentmain implements Operations{
+	public static void main(String...args[]){
+		System.out.println();
+	}
+	
+} 
