5a530740950674c458f31b81b4f4dc33ffdc5299
PPR2006 <praneethreddypolam06@gmail.com>
2025-11-30 18:34:07 +0530

    Update Lab8p1.java

--- LAB8/Lab8p1.java
+++ LAB8/Lab8p1.java
@@ -2,61 +2,63 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Lab8p1 extends Frame implements ActionListener {
-    Label label; 
-    Lab8p1(){ 
+    Label label;
+
+    Lab8p1() {
         super("FRAME");
+
         MenuBar mb = new MenuBar();
         setMenuBar(mb);
         setLayout(null);
 
-        label = new label();
+        // FIX 1: 'new label()' → 'new Label()'
+        label = new Label();
         label.setFont(new Font("Arial", Font.BOLD, 40));
         label.setBounds(150, 200, 200, 50);
-        add(label, bl.CENTRE);
 
-        Menu file = new Menu("File");
+        // FIX 2: 'bl.CENTRE' is invalid → simply use add(label);
+        add(label);
 
         String[] menus = {"File", "Edit", "Help"};
         String[][] mi = {
-            {"Open", "Save", "Close", "Rename"},
-            {"Undo", "Cut", "Copy", "Paste"},
-            {"About", "Update", "License", "Report"}
+                {"Open", "Save", "Close", "Rename"},
+                {"Undo", "Cut", "Copy", "Paste"},
+                {"About", "Update", "License", "Report"}
         };
 
-        int x=0, y=0;
+        int x = 0;
 
         for (String s : menus) {
-            y=0;
             Menu m = new Menu(s);
-            for (int i=0; i<4; i++) {
-                m.add(new MenuItem(mi [x][y]));
-                y++;
+            for (int y = 0; y < 4; y++) {
+                MenuItem item = new MenuItem(mi[x][y]);
+                item.addActionListener(this);  // FIX 3: menu items must listen
+                m.add(item);
             }
-            x++;
             mb.add(m);
+            x++;
         }
 
         setSize(500, 400);
         setVisible(true);
 
-        addWindowListener(new WindowAdapter(){
-            public void windowClosing(WindowEvent we){
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
                 dispose();
             }
         });
     }
 
     public void actionPerformed(ActionEvent ae) {
-
-        String clicked = ae.getActionCommand();  
-
+        String clicked = ae.getActionCommand();
         label.setText(clicked.toUpperCase());
 
         if (clicked.equals("Close")) {
             System.exit(0);
         }
     }
-    public static void main(String...args){
+
+    public static void main(String... args) {
         new Lab8p1();
     }
 }
